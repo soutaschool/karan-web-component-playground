72e7670677473c35aab9c06c8744932d2dc7c3a4
soutaschool <futa.development@gmail.com>
2024-12-29 09:02:25 +0900

    added dark theme to storybook preview

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,15 @@
-import type { StorybookConfig } from '@storybook/web-components-vite';
+import type { StorybookConfig } from "@storybook/web-components-vite";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@chromatic-com/storybook'],
-  framework: {
-    name: '@storybook/web-components-vite',
-    options: {},
-  },
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-themes",
+	],
+	framework: {
+		name: "@storybook/web-components-vite",
+		options: {},
+	},
 };
 export default config;
